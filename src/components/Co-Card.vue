c37366e17ef4bbf65bbfe7c0fc61a96bb3d9a3fa
Admin_Fintopia <admin_fintopia@Admin-Fintopias-MacBook-Pro.local>
2022-09-28 16:53:07 +0700

    add new card

--- src/components/Co-Card.vue
+++ src/components/Co-Card.vue
@@ -6,14 +6,58 @@
           <p class="font-thin text-xs py-6 text-gray-600">Want to preview our latest collections before they hit stores? Simply book an appointment to attend one of our exclusive designer weekends where our lucky stockists will have a whole host of new gowns for you to try on fresh from the catwalk! Attending a trunk show can have many benefits for brides such as special event discounts, expertise and an up-close look at the designers’ style and ranges, as well as getting to see a whole host of gowns the boutique may not usually stock or have room for in their boutique.</p>
         </div>
       </div>
-      <div class="py-2 bg-white">
-        <div class="2xs:mx-6 drop-shadow-lg">
-          <div class="py-4 rounded-lg text-center mx-auto bg-white">
-            <h1 class="bg-black text-white italic font-mono rounded-t-lg -mt-4 py-4 ">Ronald Joyce Designer Event</h1>
-            <h2 class="py-2 font-sans">1st - 2nd October</h2>
-            <span class="font-serif">PERFECT DAY</span>
-            <p class="py-2 font-thin text-sm text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Dark</button>
+      <div>
+        <div class="w-full py-1 bg-white 2xs:gap-y-2 2xs:gap-x-4">
+          <div class="flex scroll-mx-0 overflow-auto touch-auto scroll-pl-6 snap-x">
+
+            <div class="2xs:mx-2 drop-shadow-lg py-4">
+              <div class="py-4 px-8 rounded-lg text-center bg-white snap-start">
+                <div class="bg-slate-900 w-full rounded-b-lg">
+                  <h1 class="text-white italic font-mono -tracking-[1px] rounded-t-lg -mt-4 py-2">Ronald Joyce Designer Event</h1>
+                </div>
+                <h2 class="py-2 font-sans">1st - 2nd October</h2>
+                <span class="font-serif">PERFECT DAY</span>
+                <p class="py-2 font-thin text-xs text-gray-700 mx-2 font-mono">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:outline-none font-medium rounded-lg text-sm px-28 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Dark</button>
+              </div>
+            </div>
+
+            <div class="2xs:mx-2 drop-shadow-lg py-4">
+              <div class="py-4 px-8 rounded-lg text-center bg-white snap-center">
+                <div class="bg-black w-full rounded-b-lg">
+                  <h1 class=" text-white italic font-mono -tracking-[1px] rounded-t-lg -mt-4 py-2">Ronald Joyce Designer Event</h1>
+                </div>
+                <h2 class="py-2 font-sans">1st - 2nd October</h2>
+                <span class="font-serif">PERFECT DAY</span>
+                <p class="py-2 font-thin text-xs text-gray-700 mx-2 font-mono">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:outline-none font-medium rounded-lg text-sm px-28 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Dark</button>
+              </div>
+            </div>
+
+            <div class="2xs:mx-2 drop-shadow-lg py-4">
+              <div class="py-4 px-8 rounded-lg text-center bg-white snap-center">
+                <div class="bg-black w-full rounded-b-lg">
+                  <h1 class=" text-white italic font-mono -tracking-[1px] rounded-t-lg -mt-4 py-2">Ronald Joyce Designer Event</h1>
+                </div>
+                <h2 class="py-2 font-sans">1st - 2nd October</h2>
+                <span class="font-serif">PERFECT DAY</span>
+                <p class="py-2 font-thin text-xs text-gray-700 mx-2 font-mono">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:outline-none font-medium rounded-lg text-sm px-28 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Dark</button>
+              </div>
+            </div>
+
+            <div class="2xs:mx-2 drop-shadow-lg py-4">
+              <div class="py-4 px-8 rounded-lg text-center bg-white snap-end">
+                <div class="bg-black w-full rounded-b-lg">
+                  <h1 class=" text-white italic font-mono -tracking-[1px] rounded-t-lg -mt-4 py-2">Ronald Joyce Designer Event</h1>
+                </div>
+                <h2 class="py-2 font-sans">1st - 2nd October</h2>
+                <span class="font-serif">PERFECT DAY</span>
+                <p class="py-2 font-thin text-xs text-gray-700 mx-2 font-mono">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:outline-none font-medium rounded-lg text-sm px-28 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Dark</button>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
